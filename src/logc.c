ae6d65a5a3f54897e1774d1b9b2dae9257a6d351
Orazio Briante <orazio.briante@hotmail.it>
2012-08-31 18:42:23 +0200

    libary fix

--- src/logc.c
+++ src/logc.c
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <logc/debug.h>
+#include <logc/logc.h>
 
 #include <stdlib.h>
 #include <stdarg.h>
