7c5dcbb86c21ec1baab6a84a1e5015b1ea66f406
Orazio Briante <orazio.briante@hotmail.it>
2012-09-04 10:10:50 +0200

    fix library

--- src/logc.c
+++ src/logc.c
@@ -21,7 +21,7 @@
  * \file logc.c
  * \brief The logc functions
  *
-**/
+ **/
 
 
 #include <logc/logc.h>
@@ -41,222 +41,226 @@ extern "C"
 #endif
 
 
-/**
- * Delete a File
- * @param fileName The name of the file to delete
- *
- * @return 0 for operation success, 1 for operation failure
- * */
-int
-removeFile(const char *fileName)
-{
-	if (remove(fileName)){
-		log(INFO,"Can't delete: %s", fileName);
-		return EXIT_SUCCESS;
-	}
-	else
-		log(ERROR,"%s successfully deleted.", fileName);
+  /**
+   * Delete a File
+   * @param fileName The name of the file to delete
+   *
+   * @return 0 for operation success, 1 for operation failure
+   * */
+  int
+  removeFile(const char *fileName)
+  {
+    if (remove(fileName)){
+        log(INFO,"Can't delete: %s", fileName);
+        return EXIT_SUCCESS;
+    }
+    else
+      log(ERROR,"%s successfully deleted.", fileName);
 
 
-return EXIT_FAILURE;
+    return EXIT_FAILURE;
 
-}
+  }
 
-/**
- * Check File Dimension
- *
- * @param fileName The name of the file to check
- * @return The dimension of the file (long value)
- */
-long
-getFileSize(const char * fileName)
-{
+  /**
+   * Check File Dimension
+   *
+   * @param fileName The name of the file to check
+   * @return The dimension of the file (long value)
+   */
+  long
+  getFileSize(const char * fileName)
+  {
 
-	if (fileName)
-	{
+    if (fileName)
+      {
 
-		FILE *fp = fopen(fileName, "r");
+        FILE *fp = fopen(fileName, "r");
 
-		if (fp)
-		{
-			fseek(fp, 0L, SEEK_END);
-			long sz = ftell(fp);
-			rewind(fp);
+        if (fp)
+          {
+            fseek(fp, 0L, SEEK_END);
+            long sz = ftell(fp);
+            rewind(fp);
 
-			fclose(fp);
+            fclose(fp);
 
-			log(INFO,"File: \"%s\", Dimension: %d", fileName, sz);
+            log(INFO,"File: \"%s\", Dimension: %d", fileName, sz);
 
-			return sz;
-		}
+            return sz;
+          }
 
-	}
+      }
 
-	log(ERROR,"File: \"%s\", Dimension Unknown",fileName);
+    log(ERROR,"File: \"%s\", Dimension Unknown",fileName);
 
-	return -1;
-}
+    return -1;
+  }
 
-/**
- * Erase the File (indicated  through fileName) if the file dimension are more or equal of the declared max size .
- *
- * @param fileName The name of the file that must be checked
- * @param maxSize The max size of file
- *
- * @return 0 for operation success, 1 for operation failure
- */
-int
-checkFileSize(const char * fileName, const long maxSize)
-{
-	if (fileName)
-	{
-		long size = getFileSize(fileName);
+  /**
+   * Erase the File (indicated  through fileName) if the file dimension are more or equal of the declared max size .
+   *
+   * @param fileName The name of the file that must be checked
+   * @param maxSize The max size of file
+   *
+   * @return 0 for operation success, 1 for operation failure
+   */
+  int
+  checkFileSize(const char * fileName, const long maxSize)
+  {
+    if (fileName)
+      {
+        long size = getFileSize(fileName);
 
-		debug ("fileName: %s, size:\t%d", fileName, size);
+        debug ("fileName: %s, size:\t%d", fileName, size);
 
-		if (size >= maxSize)
-			return removeFile(fileName);
+        if (size >= maxSize)
+          return removeFile(fileName);
 
-	}
+      }
 
-	return EXIT_FAILURE;
-}
+    return EXIT_FAILURE;
+  }
 
-/**
- * Initialize the Logging process. The video stream and the file stream are set to the default value.
- *
- * @param log_mode the LogMode to use for log message
- * @param debug_mode the LogMode to use for debug message
- */
-void initLog(LogMode log_mode, LogMode debug_mode)
-{
-	video_stream = DEFAULT_VIDEO_LOG;
-	file_stream = DEFAULT_FILE_LOG;
+  /**
+   * Initialize the Logging process. The video stream and the file stream are set to the default value.
+   *
+   * @param log_mode the LogMode to use for log message
+   * @param debug_mode the LogMode to use for debug message
+   */
+  void initLog(LogMode log_mode, LogMode debug_mode)
+  {
+    video_stream = DEFAULT_VIDEO_LOG;
+    file_stream = DEFAULT_FILE_LOG;
 
-	logMode=log_mode;
-	debugMode=debug_mode;
-}
+    logMode=log_mode;
+    debugMode=debug_mode;
+  }
 
-/**
- * Open a Log File called by name.
- *
- * @param fileName The name of the Log file to open.
- *
- * @return 0 for operation success, 1 for operation failure
- */
-int
-openLogFile(const char * fileName)
-{
+  /**
+   * Open a Log File called by name.
+   *
+   * @param fileName The name of the Log file to open.
+   *
+   * @return 0 for operation success, 1 for operation failure
+   */
+  int
+  openLogFile(const char * fileName)
+  {
 
-	if (fileName)
-	{
-		debug ("fileName:\t%s", fileName);
+    if (fileName)
+      {
+        debug ("fileName:\t%s", fileName);
 
-		file_stream = fopen(fileName, "a");
+        file_stream = fopen(fileName, "a");
 
-		if (file_stream){
-			debug ("Opened \"%s\" in Append Mode", fileName);
-		return EXIT_SUCCESS;
-		}
+        if (file_stream){
+            debug ("Opened \"%s\" in Append Mode", fileName);
+            return EXIT_SUCCESS;
+        }
 
-	}
+      }
 
-	return EXIT_FAILURE;
-}
+    return EXIT_FAILURE;
+  }
 
-/**
- * Change the Video stream
- *
- * @param video the new stream
- */
-void openVideoLog(FILE *video)
-{
-video_stream=video;
-}
+  /**
+   * Change the Video stream
+   *
+   * @param video the new stream
+   */
+  void openVideoLog(FILE *video)
+  {
+    video_stream=video;
+  }
 
-/**
- * Uninitialize the Logging Process
- */
-void
-uninitLog()
-{
-	if (file_stream)
-	{
-		debug ("Closing Log File");
-		fclose(file_stream);
-	}
+  /**
+   * Uninitialize the Logging Process
+   */
+  void
+  uninitLog()
+  {
+    if (file_stream)
+      {
+        debug ("Closing Log File");
+        fclose(file_stream);
+      }
 
-	video_stream = DEFAULT_VIDEO_LOG;
-	file_stream = DEFAULT_FILE_LOG;
+    video_stream = DEFAULT_VIDEO_LOG;
+    file_stream = DEFAULT_FILE_LOG;
 
-	logMode=DISABLED_LOG;
-	debugMode=DISABLED_LOG;
+    logMode=DISABLED_LOG;
+    debugMode=DISABLED_LOG;
 
-}
+  }
 
-void
-_logWrite(FILE * output, const char *type, const char *file,
-		const char *function, int line, const char *template, va_list argp)
-{
-	time_t now;
-	struct tm tmNow;
-	char timeString[26];
+  void
+  _logWrite(FILE * output, const char *type, const char *file,
+      const char *function, int line, const char *template, va_list argp)
+  {
+    time_t now;
+    struct tm tmNow;
+    char timeString[26];
 
-	now = time(NULL );
-	localtime_r(&now, &tmNow);
-	strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &tmNow);
+    now = time(NULL );
+    localtime_r(&now, &tmNow);
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &tmNow);
 
-	fprintf(output, "%s %s (%s - %s:%i): ", timeString, type, function, file,
-			line);
-	vfprintf(output, template, argp);
-	fprintf(output, "\n");
-	fflush(output);
-}
+    fprintf(output, "%s %s (%s - %s:%i): ", timeString, type, function, file,
+        line);
+    vfprintf(output, template, argp);
+    fprintf(output, "\n");
+    fflush(output);
+  }
 
-void
-_log(const LogType logType, const char *file, const char *function, int line,
-		const char *template, ...)
-{
-
-
-	char* type=NULL;
-
-	va_list argp;
-	va_start(argp, template);
-
-	if (logType == ERROR)
-		type="ERROR  ";
-	else if (logType == WARNING)
-		type="WARNING";
-	else
-		type="INFO   ";
+  void
+  _log(const LogType logType, const char *file, const char *function, int line,
+      const char *template, ...)
+  {
 
 
-	if(video_stream && logMode!=DISABLED_LOG)
-		_logWrite(video_stream, type, file, function, line, template, argp);
+    char* type=NULL;
 
-	if (file_stream && (logMode==FILE_LOG || logMode==FILE_VIDEO_LOG))
-		_logWrite(file_stream, type, file, function, line, template, argp);
+    va_list argp;
+    va_start(argp, template);
 
-	va_end(argp);
-}
+    if (logType == ERROR)
+      type="ERROR  ";
+    else if (logType == WARNING)
+      type="WARNING";
+    else
+      type="INFO   ";
 
-void
-_debug(const char *file, const char *function, int line, const char *template,
-		...)
-{
-	va_list argp;
-	va_start(argp, template);
+    if(logMode!= DISABLED_LOG)
+      {
+        if(video_stream && debugMode!=FILE_LOG)
+          _logWrite(video_stream, type, file, function, line, template, argp);
 
-	if(video_stream && debugMode!=DISABLED_LOG)
-		_logWrite(video_stream, "DEBUG  ", file, function, line, template, argp);
+        if (file_stream && logMode!=VIDEO_LOG)
+          _logWrite(file_stream, type, file, function, line, template, argp);
+      }
 
-	if (file_stream && (debugMode==FILE_LOG || debugMode==FILE_VIDEO_LOG))
-		_logWrite(file_stream, "DEBUG  ", file, function, line, template, argp);
+    va_end(argp);
+  }
 
+  void
+  _debug(const char *file, const char *function, int line, const char *template,
+      ...)
+  {
+    va_list argp;
+    va_start(argp, template);
 
-	va_end(argp);
-}
+    if(logMode!= DISABLED_LOG)
+      {
+        if(video_stream &&  debugMode!= FILE_LOG)
+          _logWrite(video_stream, "DEBUG  ", file, function, line, template, argp);
+
+        if (file_stream &&  debugMode!= VIDEO_LOG)
+          _logWrite(file_stream, "DEBUG  ", file, function, line, template, argp);
+      }
+
+    va_end(argp);
+  }
 
 
 #ifdef __cplusplus
